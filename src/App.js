a3819dac1740b1f76d47fe9cc05fc852488bab05
robinbenitezmora <robinbenitez1990@gmail.com>
2023-05-03 20:50:43 -0500

    Add a model card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Product from './components/Product';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <h1>This is a proof</h1>
+    <Product />
     </div>
   );
 }
