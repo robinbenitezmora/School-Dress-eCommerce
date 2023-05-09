5e8dbc4a0e6637cbe47d45b301061e34c80f77d1
robinbenitezmora <robinbenitez1990@gmail.com>
2023-05-08 20:40:04 -0500

    Calculate items' price and quantity

--- main.js
+++ main.js
@@ -30,12 +30,43 @@ function ready() {
   var button = removeCartButtons[i]
   button.addEventListener('click', removeCartItem)
  }
+ // Quantity Changes
+ var quantityInputs = document.getElementsByClassName('cart-quantity');
+ for (let i = 0; i < quantityInputs.length; i++) {
+  var input = quantityInputs[i];
+  input.addEventListener("change", quantityChanged);
+ }
 }
 
 // Remoe Items From Cart
 function removeCartItem(event){
- var buttonClicked = event.target
- buttonClicked.parentElement.remove()
+ var buttonClicked = event.target;
+ buttonClicked.parentElement.remove();
+ updateTotal();
+}
+// Quantity Changes
+function quantityChanged(event) {
+ var input = event.target
+ if (isNaN(input.value) || input.value <= 0) {
+  input.value = 1;
+ }
+ updateTotal();
 }
 
-
+// Update Total
+function updateTotal() {
+ var cartContent = document.getElementsByClassName('cart-content')[0];
+ var cartBoxes = cartContent.getElementsByClassName('cart-box');
+ var total = 0;
+ for (let i = 0; i < cartBoxes.length; i++){
+  var cartBox = cartBoxes[i];
+  var priceElement = cartBox.getElementsByClassName('cart-price')[0];
+  var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+  var price = parseFloat(priceElement.innerText.replace("$", ""));
+  var quantity = quantityElement.value;
+  total += (price * quantity);
+  // If price Contain some Cents Value
+  total = Math.round(total * 100 ) / 100;
+  document.getElementsByClassName('total-price')[0].innerText = '$' + total;
+ }
+}
